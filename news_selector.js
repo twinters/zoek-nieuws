f45252f62848a1d0e792e89a02ea8aa4c4802f5a
Thomas Winters <info@thomaswinters.be>
2020-08-06 01:53:08 +0200

    Sorted all incoming articles on their publication date

--- news_selector.js
+++ news_selector.js
@@ -3,7 +3,12 @@ const vrtnu = require('./news_sources/vrt_nws');
 const newsSources = [vrtnu];
 
 async function search(topic) {
-    return (await Promise.all(newsSources.map(n => n.search(topic)))).flatMap(e=>e)
+    const allArticles = (await Promise.all(newsSources.map(n => n.search(topic)))).flatMap(e => e);
+
+    // Sort so most recent article first
+    allArticles.sort(function (a, b) {
+        return b.date - a.date;
+    });
 }
 
 exports.search = search;
