466235239057937ed240fc98f29b556b578443fe
Thomas Winters <info@thomaswinters.be>
2020-08-05 02:22:12 +0200

    Implemented main way of receiving the topic

--- bot.js
+++ bot.js
@@ -1,13 +1,34 @@
-const replyToNewMentions = require("./twitter_access").replyToNewMentions;
+const twitterAccess = require("./twitter_access");
+const replyToNewMentions = twitterAccess.replyToNewMentions;
 const executeEveryCoupleMinutes = require('./execute_loop').executeEveryCoupleMinutes;
+const newsSelector = require('./news_selector');
 
 // Dealing with run duration
 const runTimeMinutes = process.argv[2] || 1,
     checkEveryMinutes = process.argv[3] || 5;
 
 
-const mentionReplier = function (mention) {
-    return "Test";
-};
+function extractTweetText(text) {
+    const splitted = text.split(" ");
+    let firstMeaningfulWord = 0;
+    while (firstMeaningfulWord < splitted.length && splitted[firstMeaningfulWord].startsWith("@")) {
+        firstMeaningfulWord += 1;
+    }
+    return splitted.slice(firstMeaningfulWord).join(" ");
+}
+
+const maxNumberOfArticles = 5;
+
+function mentionReplier(mention) {
+    const mentionTweetTextWords = extractTweetText(mention.text);
+
+    let topic = mentionTweetTextWords;
+
+    const articles = newsSelector.search(topic, maxNumberOfArticles);
+    console.log("Found articles about ", topic, ":\n", articles);
+
+    return "Hier zijn enkele artikels over '" + topic + "':\n"
+        + articles.slice(0, maxNumberOfArticles).join("\n");
+}
 
 executeEveryCoupleMinutes(runTimeMinutes, checkEveryMinutes, replyToNewMentions(mentionReplier));
