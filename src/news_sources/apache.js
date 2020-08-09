71830b64e2719e22340eaaa0f154ae1f04a66e0f
Thomas Vanderhaeghen <thomas@optios.net>
2020-08-09 10:40:07 +0200

    Added apache news source

---
+++ src/news_sources/apache.js
@@ -0,0 +1,34 @@
+const searcherUtil = require('../searcher_util');
+const $ = require('cheerio');
+const moment = require('moment');
+
+async function search(topic) {
+    const rawData = await searcherUtil.searchRaw(`https://www.apache.be/?s=${topic}`);
+
+    if (!rawData) {
+        return []
+    }
+    const articles = [];
+    $('article.post', rawData, '#content').each((index, element) => {
+        const title = $(element).find("h3").text().trim();
+        const content = $(element).find("p:not(.archive-item-meta)").first().text().trim();
+        const link = $(element).find('a').first().attr('href');
+
+        // Time
+	const dateMatches = link.match(/(\d{4})\/(\d{2})\/(\d{2})/);
+	const time = new Date(parseInt(dateMatches[1]), parseInt(dateMatches[2]) - 1, parseInt(dateMatches[3]), 0, 0, 0);
+
+        articles.push({
+            title: title,
+            date: time,
+            content: content,
+            url: link,
+            source: "apache",
+        })
+    });
+
+    return articles;
+}
+
+
+exports.search = search;
