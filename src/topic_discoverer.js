cb670c467ee3f8405d6d5b2c7410c46992890792
Thomas Winters <info@thomaswinters.be>
2020-08-10 22:01:58 +0200

    Fixed not removing tags @ tags properly

--- src/topic_discoverer.js
+++ src/topic_discoverer.js
@@ -2,12 +2,15 @@ function extractTweetText(text) {
     if (!text) {
         return "";
     }
-    const splitted = text.split(" ").filter(word=>!word.startsWith("http"));
+    const splitted = text.split(" ");
     let firstMeaningfulWord = 0;
     while (firstMeaningfulWord < splitted.length && splitted[firstMeaningfulWord].startsWith("@")) {
         firstMeaningfulWord += 1;
     }
-    return splitted.slice(firstMeaningfulWord).join(" ");
+    return splitted.slice(firstMeaningfulWord)
+        .filter(word=>!word.startsWith("http"))
+        .filter(word=>word.indexOf("@") > 0)
+        .join(" ");
 }
 
 const bracketRegex = /["“'](.*?)["”']/;
@@ -15,7 +18,7 @@ const bracketRegex = /["“'](.*?)["”']/;
 function discoverFromMention(mention) {
     const tweetText = extractTweetText(mention.text)
         // Replace all punctuations
-        .replace(/[.,\/#!$;:{}=_`~()]/g, "");
+        .replace(/[.,\/#@!$;:{}=_`~()]/g, "");
 
     if (tweetText.match(bracketRegex)) {
         return tweetText.match(bracketRegex)[1]
