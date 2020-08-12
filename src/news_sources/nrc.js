16944e7ad9df0d4237e1c628ff1b7962c7657ea5
Thomas Winters <info@thomaswinters.be>
2020-08-13 00:35:03 +0200

    Added nrc

---
+++ src/news_sources/nrc.js
@@ -0,0 +1,37 @@
+const searcherUtil = require('../searcher_util');
+const $ = require('cheerio');
+const moment = require('moment');
+
+async function search(topic) {
+    const rawData = await searcherUtil.searchRaw(`https://www.nrc.nl/search/api/v1/article/search?query=${topic}&from=0&size=8&article_type=alle_artikelen&aggregations%5B%5D=rubric&aggregations%5B%5D=dossier`);
+
+
+    if (!rawData || !rawData.hits || !rawData.hits.length) {
+        return []
+    }
+    console.log(rawData.hits)
+
+    // Collect article objects
+    const articles = [];
+    for (const hit of rawData.hits) {
+
+        const searchResult = hit.source;
+
+        // Check if it is an article by checking if it has a date
+        if (searchResult.published_at) {
+            articles.push({
+                title: searchResult.headline,
+                date: new Date(searchResult.published_at),
+                content: searchResult.abstract,
+                url: searchResult.url,
+                source: searchResult.publication
+            })
+
+        }
+
+    }
+    return articles;
+}
+
+
+exports.search = search;
