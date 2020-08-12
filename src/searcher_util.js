bd6cfff7adb9a24064b9ec7728eb2824868cd6e6
Thomas Winters <info@thomaswinters.be>
2020-08-12 23:40:12 +0200

    Allowed for setting parameters

--- src/searcher_util.js
+++ src/searcher_util.js
@@ -1,10 +1,10 @@
 const axios = require("axios");
 
-async function searchRaw(url) {
+async function searchRaw(url, params={}) {
     let rawArticleData = null;
     await (async () => {
         try {
-            const response = await axios.get(url);
+            const response = await axios.get(url, params);
             rawArticleData = response.data;
         } catch (error) {
             console.log("error in searching ", url, error.response && error.response.body);
